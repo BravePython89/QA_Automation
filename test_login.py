f50d777eec6365888eb046586f68ceb845145807
BravePython89 <hotsmots@gmail.com>
2023-02-27 15:28:53 +0200

    some test

--- test_login.py
+++ test_login.py
@@ -3,11 +3,12 @@ from signin import SignIn
 
 @pytest.mark.login
 def test_login():
-    sign_in = SignIn()
+    while True:
+        sign_in = SignIn()
 
-    sign_in.go_to()
+        sign_in.go_to()
 
-    sign_in.login('gis1989', 'Agent88888888!')
+        sign_in.login('gis1989', 'Agent88888888!')
 
-    sign_in.logout()
+        sign_in.logout()
 
